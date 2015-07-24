9e7edd84d16a5ab66a9d30aa0e640087ad50c1a9
crobert <cr.cedric.robert@gmail.com>
2015-07-24 20:29:47 +0200

    update menu

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -12,11 +12,10 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="<?php echo site_url(); ?>">Gesti'truc</a>
+            <a class="navbar-brand" href="<?php echo site_url('collections'); ?>">Gesti'truc</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li <?php echo ($menu=='')?"class='active'":"";?>><a href="<?php echo site_url(); ?>">Home</a></li>
                 <li <?php echo ($menu=='users')?"class='active'":"";?>><a href="<?php echo site_url('users'); ?>">Users</a></li>
                 <li <?php echo ($menu=='collections')?"class='active'":"";?>><a href="<?php echo site_url('collections'); ?>">Collections</a></li>
                 <li <?php echo ($menu=='categories')?"class='active'":"";?>><a href="<?php echo site_url('categories'); ?>">Categories</a></li>
