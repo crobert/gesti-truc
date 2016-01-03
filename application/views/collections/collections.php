91a8cbd0c6b0bc1abfa0714ccfc822d33f40742f
crobert <cr.cedric.robert@gmail.com>
2016-01-03 14:51:41 +0100

    style collections list

--- application/views/collections/collections.php
+++ application/views/collections/collections.php
@@ -3,19 +3,23 @@
  * @var $collections array
  */
 ?>
-<a href="<?php echo site_url('collections/add'); ?>">Ajouter</a>
+<a href="<?php echo site_url('collections/add'); ?>">
+    <input class="btn btn-info" type="button" value="Ajouter" name="Ajouter"/>
+</a>
 <br/>
 <br/>
 
 <?php foreach ($collections as $c) { ?>
-    <a class="vignetteLink" href='<?php echo site_url('collections/detail/' . $c->id); ?>'>
-        <?php if ($c->picture != '') : ?>
-            <div class="vignette" style="background: url('<?php echo base_url('uploads/collections/' . $c->picture); ?>')">
-        <?php else : ?>
-            <div class="vignette">
-        <?php endif; ?>
-
-                <?php echo $c->name; ?>
-            </div>
-    </a>
+    <div class="vignette">
+        <a class="vignetteLink" href='<?php echo site_url('collections/detail/' . $c->id); ?>'>
+            <?php if ($c->picture != '') : ?>
+            <div class="vignette"
+                 style="background: url('<?php echo base_url('uploads/collections/' . $c->picture); ?>')">
+            <?php else : ?>
+                <div class="vignette">
+            <?php endif; ?>
+                </div>
+            <span class="titreVignette"><?php echo $c->name; ?></span>
+        </a>
+    </div>
 <?php } ?>
